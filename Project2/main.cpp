9bc894d8bfa7e7ae169ea83fbad1242fd018c12c
ArtinHoti <artinhoti08@gmail.com>
2026-01-01 20:13:01 +0100

    Kodi qe e mbledh shumen.

--- Project2/main.cpp
+++ Project2/main.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
 using namespace std;
-   
-int main() { 
-    cout << "Project2 - GitHub demo " << endl;
+
+int main() {
+    int number;
+    int sum = 0;
+
+    cout << "Enter positive numbers (0 to stop): " << endl;
+
+    cin >> number;
+
+    while (number != 0) {
+        if (number > 0) {
+            sum += number;
+        }
+        cin >> number;
+    }
+
+    cout << "Sum of entered numbers is: " << sum << endl;
+
     return 0;
 }
