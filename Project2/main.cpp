264af6505bd374c156a26b306a109773011e285b
ArtinHoti <artinhoti08@gmail.com>
2026-01-01 22:38:55 +0100

    Luck on ur side?

--- Project2/main.cpp
+++ Project2/main.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
 using namespace std;
-   
-int main() { 
-    cout << "Project2 - GitHub demo " << endl;
+
+int main() {
+    int secret = 7;
+    int guess;
+
+    cout << "Guess the number: ";
+    cin >> guess;
+
+    while (guess != secret) {
+        cout << "Wrong guess. Try again: ";
+        cin >> guess;
+    }
+
+    cout << "Correct! You guessed the number." << endl;
     return 0;
 }
+
