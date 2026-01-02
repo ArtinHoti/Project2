17df3a61f68d0db6fe184d9351bd2edfcd10f262
Art-Mehmeti <art.mehmeti@student.uni-pr.edu>
2026-01-02 21:08:04 +0100

    Implement option to repeat sum calculation
    Added functionality to repeat the sum calculation.

--- Project2/main.cpp
+++ Project2/main.cpp
@@ -4,7 +4,8 @@ using namespace std;
 int main() {
     int number;
     int sum = 0;
-
+    int again;
+    label_1:
     cout << "Enter positive numbers (0 to stop): " << endl;
 
     cin >> number;
@@ -17,6 +18,11 @@ int main() {
     }
 
     cout << "Sum of entered numbers is: " << sum << endl;
-
+    cout << "If u want to do it again type 1 if not type 0";cin >> again;
+    if(again == 1){
+        goto label_1;
+    }else{
+        return 0 ;
+    }
     return 0;
 }
