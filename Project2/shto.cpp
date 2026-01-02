b7cc91a10f8ce659e4bab8f3940ed72a3f3e9e25
ArtinHoti <artinhoti08@gmail.com>
2026-01-02 23:04:55 +0100

    Shtimi i kodit

---
+++ Project2/shto.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <cmath>   // për pow()
+using namespace std;
+
+int main() {
+    int choice;
+    double a, b;
+
+    do {
+        cout << "\n--- MENU CALCULATOR ---\n";
+        cout << "1. Add (+)\n";
+        cout << "2. Subtract (-)\n";
+        cout << "3. Multiply (*)\n";
+        cout << "4. Divide (/)\n";
+        cout << "5. Power (a^b)\n";
+        cout << "0. Exit\n";
+        cout << "Choose: ";
+        cin >> choice;
+
+        if (choice >= 1 && choice <= 5) {
+            cout << "Enter two numbers: ";
+            cin >> a >> b;
+        }
+
+        switch (choice) {
+        case 1:
+            cout << "Result = " << a + b << endl;
+            break;
+
+        case 2:
+            cout << "Result = " << a - b << endl;
+            break;
+
+        case 3:
+            cout << "Result = " << a * b << endl;
+            break;
+
+        case 4:
+            if (b != 0)
+                cout << "Result = " << a / b << endl;
+            else
+                cout << "Error: Division by zero!\n";
+            break;
+
+        case 5:
+            cout << "Result = " << pow(a, b) << endl;
+            break;
+
+        case 0:
+            cout << "Bye!\n";
+            break;
+
+        default:
+            cout << "Invalid choice! Try again.\n";
+        }
+
+    } while (choice != 0);
+
+    return 0;
+}
